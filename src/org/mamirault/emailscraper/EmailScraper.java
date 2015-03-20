6cfbddc08bc5acd97413ba3d33735ba747e20ae5
Michael Amirault <Michael@Michaels-MacBook-Air.local>
2015-03-20 10:39:13 -0400

    Initial commit

---
+++ src/org/mamirault/emailscraper/EmailScraper.java
@@ -0,0 +1,13 @@
+package org.mamirault.emailscraper;
+
+public class EmailScraper {
+	private final String domain;
+
+	EmailScraper(String domain) {
+		this.domain = domain;
+	}
+	
+	public void scrape() {
+		
+	}
+}
---
+++ src/org/mamirault/emailscraper/EmailScraperRunner.java
@@ -0,0 +1,10 @@
+package org.mamirault.emailscraper;
+
+public class EmailScraperRunner {
+
+	public static void main(String[] args) {
+		EmailScraper emailScraper = new EmailScraper(args[0]);
+	}
+	
+	
+}
